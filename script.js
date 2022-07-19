dd2e4ebc2c3b2bea330b763f728a4d35d119054e
Lawrence <locaulker@gmail.com>
2022-07-19 06:09:14 -0400

    resume @ L16 - Coding Challenge

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ let continent = "North America"
 let population = "350M"
 
 console.log(country, continent, population)
+
+let x = 5
+x += 10 // x = x + 5
+console.log(x)
+//x -= 10 // x = x - 10
+// console.log(x)
