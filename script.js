ec5467641238586ab5bd20694167c7a038952021
Lawrence O. Caulker <locaulker@gmail.com>
2022-07-19 13:01:04 -0400

    end of coding challenge #1

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-let js = "amazing"
-console.log(40 + 8 + 23 - 10)
+// let js = "amazing"
+// console.log(40 + 8 + 23 - 10)
 
-let country = "USA"
-let continent = "North America"
-let population = "350M"
+// let country = "USA"
+// let continent = "North America"
+// let population = "350M"
 
-console.log(country, continent, population)
+// console.log(country, continent, population)
+
+// Coding Challenge 1
+
+const massMark = 78
+const heightMark = 1.69
+
+const massJohn = 92
+const heightJohn = 1.95
+
+const bmiMark = Math.round(massMark / heightMark ** 2).toFixed(2)
+const bmiJohn = Math.round(massJohn / heightJohn ** 2).toFixed(2)
+
+const markHigherBMI = bmiMark - bmiJohn
+if (bmiMark > bmiJohn) {
+  console.log(bmiMark, bmiJohn, true)
+} else {
+  console.log(false)
+}
+
+console.log(bmiMark, bmiJohn)
