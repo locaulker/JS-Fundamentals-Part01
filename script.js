a5a3b2b03de59be790087ed3c3bb06bbbaebd26c
Lawrence O. Caulker <locaulker@gmail.com>
2022-07-20 20:27:00 -0400

    end JS fundamentals Part 01

--- script.js
+++ script.js
@@ -9,20 +9,20 @@
 
 // Coding Challenge 1
 
-const massMark = 78
-const heightMark = 1.69
+// const massMark = 78
+// const heightMark = 1.69
 
-const massJohn = 92
-const heightJohn = 1.95
+// const massJohn = 92
+// const heightJohn = 1.95
 
-const bmiMark = Math.round(massMark / heightMark ** 2).toFixed(2)
-const bmiJohn = Math.round(massJohn / heightJohn ** 2).toFixed(2)
+// const bmiMark = Math.round(massMark / heightMark ** 2).toFixed(2)
+// const bmiJohn = Math.round(massJohn / heightJohn ** 2).toFixed(2)
 
-const markHigherBMI = bmiMark - bmiJohn
-if (bmiMark > bmiJohn) {
-  console.log(bmiMark, bmiJohn, true)
-} else {
-  console.log(false)
-}
+// const markHigherBMI = bmiMark - bmiJohn
+// if (bmiMark > bmiJohn) {
+//   console.log(bmiMark, bmiJohn, true)
+// } else {
+//   console.log(false)
+// }
 
-console.log(bmiMark, bmiJohn)
+// console.log(bmiMark, bmiJohn)
